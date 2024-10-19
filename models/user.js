ee48e3b79a870034b0a65dafc93e71890fdd5bd7
Mohammad Imran <79560811+Mohammad-Imran786@users.noreply.github.com>
2024-10-19 19:42:33 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/test_app")
+mongoose.connect("mongodb://127.0.0.1:27017/your_database_name")
 
 const userSchema = mongoose.Schema({
     name: String,
